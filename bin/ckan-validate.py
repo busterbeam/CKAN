20ae9953057f3e6ec1ece4880bbfe6f975aceb1b
Nath <nathanlloyd@protonmail.com>
2021-10-12 19:48:14 +0100

    Update ckan-validate.py
    This should work haven't run it myself.  assuming you use https://pypi.org/project/jsonschema/ vesion of jsonschema.

--- bin/ckan-validate.py
+++ bin/ckan-validate.py
@@ -1,58 +1,50 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
-import os, sys
+from os.path import exists
+from sys import argv, exit
+from json import load
 
-import json
 from jsonschema import validate, ValidationError
 
-def main():
-    if len(sys.argv) == 1:
-        print 'Usage:'
-        print sys.argv[0] + ' <.ckan files>'
-        sys.exit(0)
-        
-    SCHEMA_PATH = "CKAN.schema"
-    if not os.path.exists(SCHEMA_PATH):
-        print 'Cannot find JSON schema at %s' % SCHEMA_PATH
-        SCHEMA_PATH = "../" + SCHEMA_PATH
-        if not os.path.exists(SCHEMA_PATH):
-            print 'Cannot find JSON schema at %s' % SCHEMA_PATH
-            sys.exit(1)
-        
-    schema = None
-        
-    with open(SCHEMA_PATH, 'r') as schema_file:
-        schema = json.load(schema_file)
-        
-    if schema == None:
-        print 'Could not parse JSON schema, exiting..'
-        sys.exit(1)
-        
-    files = sys.argv[1:]
-    error = 0
-    
-    for ckan_path in files:
-        if not os.path.exists(ckan_path):
-            print 'File "%s" does not exist, skipping..' % ckan_path
-            continue
-        
-        with open(ckan_path, 'r') as ckan_file:
-            print 'Validating %s..' % ckan_path,
-            try:
-                validate(json.load(ckan_file), schema)
-            except ValidationError as e:
-                print 'Failed! See below for error description.'
-                print e
-                error = 1
-                continue
-            except ValueError as e:
-                print 'Failed! This error will be cryptic, but often a JSON or property error'
-                print e
-                error = 1
-                continue
-            print 'Success!'
-
-    sys.exit(error)
-
 if __name__ == "__main__":
-    main()
+	if len(argv) == 1:
+		print(f"Usage: {argv[0]} <.ckan files>")
+		exit(0)
+	
+	SCHEMA_PATH = "CKAN.schema"
+	if not exists(SCHEMA_PATH):
+		print(f"Cannot find JSON schema at {SCHEMA_PATH}")
+		SCHEMA_PATH = "../{SCHEMA_PATH}"
+		if not exists(SCHEMA_PATH):
+			print("Cannot find JSON schema at {SCHEMA_PATH}")
+			while not exists(SCHEMA_PATH):
+				SCHEMA_PATH = input(" Where is it?  Empty to exit\n > ")
+				if not SCHEMA_PATH:
+					exit(1)
+	
+	schema = None
+	with open(SCHEMA_PATH) as schema_file:
+		schema = load(schema_file)
+	
+	if not isinstance(schema, dict):
+		print("Could not parse JSON schema, exiting..")
+		exit(1)
+	
+	files, total = argv[1:], len(argv[1:])
+	
+	for num, ckan_path in enumerate(files, start=1):
+		print(end="{'*' * num/total*80:<80} \r")
+		if not exists(ckan_path):
+			print(f"\x1B[2K File \"{ckan_path}\" does not exist, skipping..")
+			continue
+		
+		with open(ckan_path, 'r') as ckan_file:
+			print(f"\x1B[2K Validating {ckan_path}..")
+			try: validate(instance=load(ckan_file), schema=schema)
+			except ValidationError as error:
+				print(f"\x1B[2K Failed! {error}")
+				continue
+			except ValueError as error:
+				print(f"\x1B[2K Failed! JSONPropertyError? {error}")
+				continue
+			print("\x1B[2K Success!")
